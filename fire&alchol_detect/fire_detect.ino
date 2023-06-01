aeedffae3b58543a8f9f93e0cc045b72d21f97d0
junho <masterjunho@gmail.com>
2023-06-01 22:53:59 +0900

    fire detect complete

--- fire&alchol_detect/fire_detect.ino
+++ fire&alchol_detect/fire_detect.ino
@@ -1,65 +1,76 @@
+// 필요한 라이브러리를 포함합니다.
 #include <SoftwareSerial.h>
 
-//공기질 센서에서 받은 값이 임계값을 넘었을 경우, 불꽃 감지 센서 감지됐을 경우 LED를 키고 부저를 울리며 지그비모듈로 fire신호를 보냄
-
+// 핀 번호를 정의합니다.
 #define MQ135_PIN1 A0
 #define MQ135_PIN2 A1
-#define BUZZ = 6
-#define FLAME_SENSOR_PIN1 7
-#define FLAME_SENSOR_PIN2 8
-#define LED 13
-#define AIR_QUALITY_THRESHOLD 200 // 공기질 임계값
-#define FLAME_THRESHOLD 500		  // 불꽃 감지 임계값
+#define BUZZ_PIN 6
+#define FLAME_SENSOR_PIN 7
+#define LED1 13
+#define LED2 12
+#define FLAME_THRESHOLD 500
 
-// ZigBee 모듈을 연결할 시리얼 핀 설정
-SoftwareSerial zigbeeSerial(10, 11);  // RX, TX
+SoftwareSerial zigbeeSerial(10, 11);
+int flame_state = 0; // 감지하면 0
+int air_state = 0;
 
-void setup()
-{
-	zigbeeSerial.begin(9600);   // ZigBee 모듈 시리얼 통신 시작
-	pinMode(FLAME_SENSOR_PIN1, INPUT);
-	pinMode(FLAME_SENSOR_PIN2, INPUT);
-	pinMode(LED, OUTPUT);
+
+
+// 임계값을 설정합니다.
+const int threshold = 17; // 화재 감지를 위한 임계값
+
+void setup() {
+  // 시리얼 통신을 설정합니다.
+  Serial.begin(9600);
+  zigbeeSerial.begin(9600);
+  pinMode(MQ135_PIN1, INPUT);
+  pinMode(MQ135_PIN2, INPUT);
+  pinMode(FLAME_SENSOR_PIN, INPUT);
+  pinMode(LED1, OUTPUT); // 공기질
+  pinMode(LED2, OUTPUT); // 불꽃
+  pinMode(BUZZ_PIN, OUTPUT);
 }
 
-void buzer()
+void buzz()
 {
-	analogWrite(BUZZ, 50);
-	delay(300);
-	analogWrite(BUZZ, 0);
-	delay(50);
+  analogWrite(BUZZ_PIN, 50);
+  delay(300);
+  analogWrite(BUZZ_PIN, 0);
+  delay(50);
 }
 
-void loop()
-{
-	digitalWrite(LED, LOW);
-	// 공기질 데이터 수집
-	int airQualityValue1 = analogRead(MQ135_PIN1);
-	int airQualityValue2 = analogRead(MQ135_PIN2);
-	
+void loop() {
+  // 두 개의 센서 값을 읽어옵니다.
+  flame_state=digitalRead(FLAME_SENSOR_PIN);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
 
-	int mean_air = (airQualityValue1 + airQualityValue2) / 2;
+  int airQualityValue1 = analogRead(MQ135_PIN1);
+  int airQualityValue2 = analogRead(MQ135_PIN2);
 
-	if (mean_air > AIR_QUALITY_THRESHOLD)
-	{
-		// 시리얼 통신을 통해 공기질 데이터 보내기
-		digitalWrite(LED, HIGH);
-		zigbeeSerial.print("fire"); //상황
-		buzer()
-	}
+  // 센서 값을 시리얼 모니터에 출력합니다.
+  Serial.print("Air Sensor Value 1: ");
+  Serial.println(airQualityValue1);
+  Serial.print("Air Sensor Value 2: ");
+  Serial.println(airQualityValue2);
+  
+  // 두 센서 값의 평균을 계산합니다.
+   int mean_air = (airQualityValue1 + airQualityValue2) / 2;
 
-	// 불꽃 감지
-	int flameValue1 = digitalRead(FLAME_SENSOR_PIN1);
-	int flameValue2 = digitalRead(FLAME_SENSOR_PIN2);
+  // 평균 값이 임계값보다 크면 LED를 켭니다.
+  if (mean_air > threshold) {
+    digitalWrite(LED1, HIGH);
+    buzz();
+    zigbeeSerial.print("fire");
+  }
+  digitalWrite(LED1, LOW);
 
-    int mean_flame = flameValue1 & flameValue2;
-
-	if (mean_flame == HIGH) {
-		// 시리얼 통신을 통해 불꽃 감지 여부 보내기
-		digitalWrite(LED, HIGH);
-		zigbeeSerial.print("fire"); //상황
-		buzer()
-	}
-
-	delay(1000); // 일정 시간 간격으로 반복 실행
+  if(flame_state == 0){
+    digitalWrite(LED2, HIGH);
+    buzz();
+    zigbeeSerial.print("fire");
+  }
+  digitalWrite(LED2, LOW);
+  // 잠시 대기합니다.
+  delay(2000);
 }
